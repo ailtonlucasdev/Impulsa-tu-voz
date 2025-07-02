ba3057657628dad2d0d57850d7c23256da75eccc
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-02 20:23:22 -0300

    adicionando menu toogle, responsividade na navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
-import './navbar.css'; // Importa nosso arquivo de estilos
-import { FaCog } from 'react-icons/fa'; // Importa o ícone de engrenagem
+import React, { useState } from 'react';
+import './navbar.css'; 
+import { FaCog, FaBars, FaTimes } from 'react-icons/fa'; // importando icones
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Função para fechar o menu ao clicar em um link (útil no mobile)
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="navbar-container">
+      <div className="navbar-content">
+        <div className="navbar-logo">
+          <a href="/" onClick={closeMenu}>NOSSOLOGO</a>
+        </div>
 
-      {/* Logo à Esquerda */}
-      <div className="navbar-logo">
-        <a href="/">NOSSOLOGO</a>
+        {/* O menu de navegação que se adapta */}
+        <nav className={isMenuOpen ? 'navbar-links active' : 'navbar-links'}>
+          <a href="/lugares" onClick={closeMenu}>Lugares</a>
+          <a href="/cursos" onClick={closeMenu}>Cursos</a>
+          <a href="/sobre" onClick={closeMenu}>Sobre</a>
+          <a href="/contatos" onClick={closeMenu}>Contatos</a>
+          <a href="/login" className="login-button-mobile" onClick={closeMenu}>Iniciar sesión</a>
+        </nav>
+
+        {/* Ações da versão desktop */}
+        <div className="navbar-actions">
+          <button className="login-button">Iniciar sesión</button>
+          <a href="/configuracoes" className="settings-icon">
+            <FaCog size={28} />
+          </a>
+        </div>
+        
+        {/* Ícone de menu para mobile */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes size={25} /> : <FaBars size={25} />}
+        </div>
       </div>
-
-      {/* Links de navegação no Centro */}
-      <nav className="navbar-links">
-        <a href="/lugares">Lugares</a>
-        <a href="/cursos">Cursos</a>
-        <a href="/sobre">Sobre</a>
-        <a href="/contatos">Contatos</a>
-      </nav>
-
-      {/* CTA e preferências à Direita */}
-      <div className="navbar-actions">
-        <button className="login-button">Iniciar sesión</button>
-        <a href="/configuracoes" className="settings-icon">
-          <FaCog size={28} /> {/* Usando o componente do ícone */}
-        </a>
-      </div>
-
     </header>
   );
 }
