c534e6152593021ae38f38121a4d152c2c80d1d6
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-01 22:31:46 -0300

    refazendo a navbar no projeto

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import "./navbar.css"
+import React from 'react';
+import './navbar.css'; // Importa nosso arquivo de estilos
+import { FaCog } from 'react-icons/fa'; // Importa o ícone de engrenagem
 
-function Navbar(){
+function Navbar() {
+  return (
+    <header className="navbar-container">
 
-    return(
-        <header>
-            <h3>
-                logo
-            </h3>
-            
+      {/* Logo à Esquerda */}
+      <div className="navbar-logo">
+        <a href="/">NOSSOLOGO</a>
+      </div>
 
+      {/* Links de navegação no Centro */}
+      <nav className="navbar-links">
+        <a href="/lugares">Lugares</a>
+        <a href="/cursos">Cursos</a>
+        <a href="/sobre">Sobre</a>
+        <a href="/contatos">Contatos</a>
+      </nav>
 
-        </header>
+      {/* CTA e preferências à Direita */}
+      <div className="navbar-actions">
+        <button className="login-button">Iniciar sesión</button>
+        <a href="/configuracoes" className="settings-icon">
+          <FaCog size={28} /> {/* Usando o componente do ícone */}
+        </a>
+      </div>
 
-    );
+    </header>
+  );
 }
 
 export default Navbar;
