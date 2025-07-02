cf024cb87738249bdc4ca29914475c1d1ec299ff
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-02 19:53:25 -0300

    adicionando hero section e area de especialidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-// src/App.jsx
-
-import Navbar from './components/navbar/navbar.jsx'; // Importe o componente que criamos
-import './App.css'; // Estilos globais (pode deixar, mas nosso foco é o da navbar)
+import Navbar from './components/navbar/navbar.jsx';
+import HeroSection from './components/HeroSection/HeroSection.jsx';
+import SpecialtiesSection from './components/SpecialtiesSection/SpecialtiesSection.jsx';
+import './App.css'; // Estilos globais
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Use o componente aqui! */}
-      
-      {/* O resto do conteúdo do seu site virá abaixo da navbar */}
-      <main style={{ padding: '2rem' }}>
-        <h1>Página Principal</h1>
-        <p>O conteúdo da sua página começa aqui.</p>
+      <Navbar/>
+      <main style={{ padding: '0.1rem' }}>
+        <HeroSection></HeroSection>
+        <SpecialtiesSection></SpecialtiesSection>
       </main>
     </div>
   );
