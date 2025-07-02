703cdf4ff3e7a6e383b53ad982862c0173287be0
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-01 21:46:16 -0300

    criando a navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css"
+
+function Navbar(){
+
+    return(
+        <header>
+            <h3>
+                logo
+            </h3>
+            
+
+
+        </header>
+
+    );
+}
+
+export default Navbar;
