a8aec7c6c1a3fc6bb281930c10cb6b403d5f51cb
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-03 16:55:27 -0300

    Adicionando features, partners e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
+// src/App.jsx
+
 import React from 'react';
+import './App.css';
+
+// Importação de todos os componentes
 import Navbar from './components/Navbar/Navbar.jsx';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
 import SpecialtiesSection from './components/SpecialtiesSection/SpecialtiesSection.jsx';
 import RecommendedCourses from './components/RecommendedCourses/RecommendedCourses.jsx';
-
-// Importação do arquivo de estilo global
-import './App.css';
+import PlatformFeatures from './components/PlatformFeatures/PlatformFeatures.jsx';
+import PartnersMarquee from './components/PartnersMarquee/PartnersMarquee.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
-    // O div principal que envolve toda a aplicação
     <div className="App">
-      
-      {/* O cabeçalho da página, que é o nosso componente Navbar */}
       <Navbar />
-      
-      {/* A tag <main> envolve todo o conteúdo principal da página */}
       <main>
         <HeroSection />
         <SpecialtiesSection />
         <RecommendedCourses />
+        <PlatformFeatures />
+        <PartnersMarquee />
       </main>
-      
-      {/* Um bom próximo passo seria criar e adicionar um componente de rodapé aqui.
-        Exemplo: <Footer /> 
-      */}
-      
+      <Footer />
     </div>
   );
 }
