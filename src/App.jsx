8c659d8ce35e418c959ce5d1f439da731444dccb
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-03 15:45:02 -0300

    renomeando e rejustando a seção de cursos recomendados

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
 import HeroSection from './components/HeroSection/HeroSection.jsx';
 import SpecialtiesSection from './components/SpecialtiesSection/SpecialtiesSection.jsx';
-import RecommendedCourses from './components/RecommendedCourse/RecommendedCourses.jsx';
-import './App.css'; // Importação do seu CSS global.
+import RecommendedCourses from './components/RecommendedCourses/RecommendedCourses.jsx';
+
+// Importação do arquivo de estilo global
+import './App.css';
 
 function App() {
   return (
+    // O div principal que envolve toda a aplicação
     <div className="App">
-      <Navbar />{}
+      
+      {/* O cabeçalho da página, que é o nosso componente Navbar */}
+      <Navbar />
+      
+      {/* A tag <main> envolve todo o conteúdo principal da página */}
       <main>
         <HeroSection />
         <SpecialtiesSection />
-        <RecommendedCourses></RecommendedCourses>
+        <RecommendedCourses />
       </main>
+      
+      {/* Um bom próximo passo seria criar e adicionar um componente de rodapé aqui.
+        Exemplo: <Footer /> 
+      */}
+      
     </div>
   );
 }
