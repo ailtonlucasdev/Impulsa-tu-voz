8179c852a401807ec26c68366a710521658a7c8e
ailtonlucasdev <ailtonlucasdev@gmail.com>
2025-07-04 21:04:46 -0300

    adicionando botões de iniciar e registrar - desktop e mobile sem style e link completos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,19 +29,25 @@ function Navbar({ onLoginClick }) {
         </div>
 
         <nav className={isMenuOpen ? 'navbar-links active' : 'navbar-links'}>
-          <a href="/lugares" onClick={closeMenu}>Lugares</a>
+          <a href="/lugares" onClick={closeMenu}>Areas</a>
           <a href="/cursos" onClick={closeMenu}>Cursos</a>
-          <a href="/sobre" onClick={closeMenu}>Sobre</a>
-          <a href="/contatos" onClick={closeMenu}>Contatos</a>
+          <a href="/sobre" onClick={closeMenu}>Suporte</a>
+          <a href="/contatos" onClick={closeMenu}>Sobre</a>
           {/* 3. Use a nova função aqui para o botão mobile */}
           <a href="#" className="login-button-mobile" onClick={handleLoginClick}>Iniciar sesión</a>
+          <a href="#" className="login-button-mobile" onClick={handleLoginClick}>Registra-se</a>
         </nav>
+        
 
         <div className="navbar-actions">
           {/* 2. Adicione o onClick aqui para o botão desktop */}
           <button className="login-button" onClick={onLoginClick}>Iniciar sesión</button>
           <a href="/configuracoes" className="settings-icon">
-            <FaCog size={28} />
+          
+          </a>
+          <button className="login-button" onClick={onLoginClick}>Registrar-se</button>
+          <a href="/configuracoes" className="settings-icon">
+            <FaCog size={28} /> {/* icone de configurações */}
           </a>
         </div>
         
